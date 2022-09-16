f3cc810b3699e3f8e494230894d7ac280ad66cf0
kannanhub <113693850+kannanhub@users.noreply.github.com>
2022-09-16 22:59:00 +0530

    Update importMultipleValues.mjs

--- importMultipleValues.mjs
+++ importMultipleValues.mjs
@@ -1,4 +1,4 @@
-import { myArray, bulb, fun } from "./exportMultipleValues.mjs";
+import { myArray, bulb, multiplyByFour } from "./exportMultipleValues.mjs";
 console.log(myArray);
 console.log(bulb);
 console.log(fun(2));
